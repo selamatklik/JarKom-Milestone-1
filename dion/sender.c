397e8c47080dd16ec4a7b074858f2c3069999a83
dionsaputra <dionsaputra666@gmail.com>
2018-11-01 20:24:43 +0700

    read_file

--- dion/sender.c
+++ dion/sender.c
@@ -9,6 +9,8 @@
 #include <netinet/in.h> 
 #include <netdb.h>
 
+#define BUFFER_LEN 512
+
 int sock_fd, n, len, dest_port, buffer_size;
 struct sockaddr_in server_address; 
 struct hostent *dest_hostnet;
@@ -45,15 +47,40 @@ void receive_ack() {
 	printf("Server : %s\n", buffer); 
 }
 
+void read_file(char* message, char *file_name) {
+	if (access(file_name, F_OK) == -1) {
+		printf("File not found\n");
+		exit(EXIT_FAILURE);
+	} else {
+		FILE *file = fopen(file_name,"r");
+		char c = fgetc(file); int i=0;
+		while (c != EOF && i < BUFFER_LEN) {
+			// printf("%c",c);
+			message[i] = c;
+			i++; c = fgetc(file);
+		}
+
+		printf("Read file completed\n");
+		fclose(file);
+	}
+}
+
 int main(int argc, char* argv[]) { 
+	char *file_name;
+	char message[BUFFER_LEN];
+
 	if (argc == 6) {
+		file_name = argv[1];
 		buffer_size = atoi(argv[3]);
 		dest_hostnet = gethostbyname(argv[4]);
 		dest_port = atoi(argv[5]);
 	} else {
 		perror("Command format: ./sendfile <filename> <windowsize> <buffersize> <destination_ip> <destination_port>");
-		return 1;
+		exit(EXIT_FAILURE);
 	}
+	
+	read_file(message,file_name);
+	printf("%s\n",message);
 
 	create_socket();
 	fill_server_info();
@@ -64,7 +91,7 @@ int main(int argc, char* argv[]) {
 		receive_ack();
 		i++;
 	}
-	 
+	
 	close(sock_fd); 
 	return 0; 
 } 
